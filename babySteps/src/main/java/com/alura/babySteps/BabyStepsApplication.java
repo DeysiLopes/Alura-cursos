2976839991d597772dfaa83683c19336c0af55df
DeysiLopes <deysilinconl@gmail.com>
2022-08-04 17:54:40 -0300

    Create controller package in the app BabySteps

--- babySteps/src/main/java/com/alura/babySteps/BabyStepsApplication.java
+++ babySteps/src/main/java/com/alura/babySteps/BabyStepsApplication.java
@@ -8,7 +8,6 @@ public class BabyStepsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BabyStepsApplication.class, args);
-		System.out.println("Oii gata");
 	}
 
 }
