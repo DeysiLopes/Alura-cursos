3af3c425cac05d8e3fea1f5dd7b6c4a8a6b74ae4
DeysiLopes <deysilinconl@gmail.com>
2022-08-04 19:24:16 -0300

    Create endpoints in the app

--- babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
+++ babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
@@ -1,13 +1,21 @@
 package com.alura.babySteps.controller;
 
-import org.springframework.stereotype.Controller;
+import com.alura.babySteps.controller.dto.TopicoDto;
+import com.alura.babySteps.modelo.Curso;
+import com.alura.babySteps.modelo.Topico;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
 
     @RequestMapping("/topicos")
+    public List<TopicoDto> lista() {
+        Topico topico = new Topico("Dúvida", "Duvida conteudo", new Curso("Sring", "Programação"));
 
+        return Arrays.asList(topico, topico, topico);
+    }
 }
