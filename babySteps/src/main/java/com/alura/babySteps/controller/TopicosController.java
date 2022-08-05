0c2ec623277a799eff2c6d9280ae4af538c12f31
DeysiLopes <deysilinconl@gmail.com>
2022-08-05 16:34:15 -0300

    Fix injection problem

--- babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
+++ babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
@@ -1,21 +1,31 @@
 package com.alura.babySteps.controller;
 
 import com.alura.babySteps.controller.dto.TopicoDto;
+import com.alura.babySteps.controller.form.TopicoForm;
 import com.alura.babySteps.modelo.Topico;
+import com.alura.babySteps.repository.CursoRepository;
 import com.alura.babySteps.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
 
-    @RequestMapping("/topicos")
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
+    }
+
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
         if (nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
