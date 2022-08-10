982f52d73312dcd4c45a5b8d8131170552c01924
DeysiLopes <deysilinconl@gmail.com>
2022-08-10 19:51:32 -0300

    feat(perfil): create Perfil class to grant access authority

---
+++ babySteps/src/main/java/com/alura/babySteps/modelo/Perfil.java
@@ -0,0 +1,23 @@
+package com.alura.babySteps.modelo;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+}
