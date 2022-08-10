58fc05a8e6d69259f867d3a8d056661b9d04411a
DeysiLopes <deysilinconl@gmail.com>
2022-08-10 15:49:39 -0300

    refactor(pagination): add Pageble in the API for better navigation

--- babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
+++ babySteps/src/main/java/com/alura/babySteps/controller/TopicosController.java
@@ -8,6 +8,9 @@ import com.alura.babySteps.modelo.Topico;
 import com.alura.babySteps.repository.CursoRepository;
 import com.alura.babySteps.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,7 +18,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -38,12 +40,14 @@ public class TopicosController {
     }
 
     @GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtd) {
+        Pageable paginacao = PageRequest.of(pagina, qtd);
+
         if (nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         }
     }
