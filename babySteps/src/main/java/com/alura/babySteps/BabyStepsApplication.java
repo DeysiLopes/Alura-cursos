ed96d3ebd217bc5fc02c3732ba4d9de84d7edfed
DeysiLopes <deysilinconl@gmail.com>
2022-08-10 16:24:33 -0300

    refactor(websupport): enable Spring Web support in the API

--- babySteps/src/main/java/com/alura/babySteps/BabyStepsApplication.java
+++ babySteps/src/main/java/com/alura/babySteps/BabyStepsApplication.java
@@ -2,12 +2,14 @@ package com.alura.babySteps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class BabyStepsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BabyStepsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BabyStepsApplication.class, args);
+    }
 
 }
