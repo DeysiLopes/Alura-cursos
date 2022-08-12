1e2748d6762d26574435e71e7ba9a216b911d9c1
DeysiLopes <deysilinconl@gmail.com>
2022-08-12 14:23:18 -0300

    feat(role): add roles in db

--- babySteps/src/main/resources/data.sql
+++ babySteps/src/main/resources/data.sql
@@ -1,5 +1,17 @@
 INSERT INTO USUARIO(nome, email, senha)
 VALUES ('Aluno', 'aluno@email.com', '$2a$10$hWmJfPmYvdjyOHDyDu2VAe5knQ8EX5juToxXFTkPBTWSzaHIDNuMm');
+INSERT INTO USUARIO(nome, email, senha)
+VALUES ('Moderador', 'moderador@email.com', '$2a$10$hWmJfPmYvdjyOHDyDu2VAe5knQ8EX5juToxXFTkPBTWSzaHIDNuMm');
+
+INSERT INTO PERFIL(id, nome)
+VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome)
+VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id)
+VALUES (1, 1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id)
+VALUES (2, 2);
 
 INSERT INTO CURSO(nome, categoria)
 VALUES ('Spring Boot', 'Programação');
